77fe5bcb63b6312f860042426400d74270d1dfdb
Massimo Calderoni <max.calderoni@gmail.com>
2023-06-14 07:28:09 +0200

    Facility: add facility view by name
    Next todos:
    - pass the correct reservation details in the message
    - check idempotency of REST calls
    - double check domain-wide auth with Google
    - check if effects().asyncReply is good

---
+++ facility/src/main/java/com/rez/facility/view/FacilityByNameView.java
@@ -0,0 +1,61 @@
+package com.rez.facility.view;
+
+import com.rez.facility.api.FacilityEntity;
+import com.rez.facility.api.FacilityEvent;
+import kalix.javasdk.view.View;
+import kalix.javasdk.annotations.Query;
+import kalix.javasdk.annotations.Subscribe;
+import kalix.javasdk.annotations.Table;
+import kalix.javasdk.annotations.ViewId;
+import reactor.core.publisher.Flux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@ViewId("view_facilities_by_name")
+@Table("facilities_by_name")
+public class FacilityByNameView extends View<Facility> {
+
+    @GetMapping("/facility/by_name/{facility_name}")
+    @Query("SELECT * FROM facilities_by_name WHERE name = :facility_name")
+    public Flux<Facility> getFacility(String name) {
+        return null;
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.Created created) {
+        return effects().updateState(new Facility(
+                created.facility().name(),
+                created.facility().address().toAddressState(),
+                created.entityId()));
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.Renamed event) {
+        return effects().updateState(viewState().withName(event.newName()));
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.AddressChanged event) {
+        return effects().updateState(viewState().withAddress(event.address().toAddressState()));
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.ResourceSubmitted event) {
+        return effects().ignore();
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.ResourceIdAdded event) {
+        return effects().ignore();
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.ResourceIdRemoved event) {
+        return effects().ignore();
+    }
+
+    @Subscribe.EventSourcedEntity(FacilityEntity.class)
+    public UpdateEffect<Facility> onEvent(FacilityEvent.ReservationCreated event) {
+        return effects().ignore();
+    }
+}
