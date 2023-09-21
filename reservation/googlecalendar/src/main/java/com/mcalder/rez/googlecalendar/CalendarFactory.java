22f0c8d1b8d1198d3bdf09d652aa5ea3df7971f0
Massimo Calderoni <max.calderoni@gmail.com>
2023-09-21 17:08:29 +0200

    Modules: add module googlecalendar.

--- reservation/googlecalendar/src/main/java/com/mcalder/rez/googlecalendar/CalendarFactory.java
+++ reservation/googlecalendar/src/main/java/com/mcalder/rez/googlecalendar/CalendarFactory.java
@@ -1,4 +1,4 @@
-package com.rez.facility.externals;
+package com.mcalder.rez.googlecalendar;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
