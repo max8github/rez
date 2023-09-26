3afab16095b500dae1eab79cb4e3b58a41f60dab
Massimo Calderoni <max.calderoni@gmail.com>
2023-09-26 10:22:12 +0200

    Parser: correct string parser implementation, add awaitility test dependency.
    Now supports also full ISO format for date and time.
    Reservation had changed to include ISO time.
    This commit includes all changes made in branch workflow except changes relative to the very Workflow (which will need to be merged or included if I want to really change ReservationEntity into a ReservationWorkflow kind of entity.

--- reservation/stringparser/src/test/java/com/mcalder/rez/stringparser/StringNlpTest.java
+++ reservation/stringparser/src/test/java/com/mcalder/rez/stringparser/StringNlpTest.java
@@ -3,15 +3,27 @@ package com.mcalder.rez.stringparser;
 import com.mcalder.rez.spi.Nlp;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringNlpTest {
 
     @Test
+    void parseISO() {
+        String content = "2023-04-12T14:00, Toni, Meg";
+        Nlp parser = new StringNlpParser();
+        Nlp.Result result = parser.parse(content);
+        assertThat(result.who()).hasSameElementsAs(Set.of("Toni", "Meg"));
+        assertEquals(result.when(), "2023-04-12T14:00");
+    }
+
+    @Test
     void parse() {
         String content = "2023-04-12, 14, Toni";
-        Nlp nlp = new StringNlpParser();
-        Nlp.Result rez = nlp.parse(content);
+        Nlp parser = new StringNlpParser();
+        Nlp.Result rez = parser.parse(content);
         assertEquals(rez.who().size(), 1);
         assertEquals(rez.who().iterator().next(), "Toni");
         assertEquals(rez.when(), "2023-04-12T14:00");
